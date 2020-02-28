b36c0ef4e71ee346e52bfa81717e9a634f13ba29
Jason Smith <eastcoastj@gmail.com>
2020-02-27 22:57:42 -0800

    ignore walls

--- src/entities/FireballScrollEntity.gd
+++ src/entities/FireballScrollEntity.gd
@@ -45,6 +45,8 @@ func create_fire(target_map_pos: Vector2) -> void:
 	var tiles : Dictionary = _field_of_view.refresh(target_map_pos, aoe_radius)
 	tiles[target_map_pos] = true
 	for map_pos in tiles.keys():
+		if globals.board.is_wall(map_pos):
+			continue
 		var fire = FireEntityScene.instance()
 		fire.set_map_pos(map_pos)
 		globals.environmental_effect_area.add_or_replace(fire)
